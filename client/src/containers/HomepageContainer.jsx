a351d36daf30adc4db993f0b9cbb002c44a4d9ad
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-04-10 17:10:35 +0100

    added logo

--- client/src/containers/HomepageContainer.jsx
+++ client/src/containers/HomepageContainer.jsx
@@ -1,6 +1,11 @@
+import logo from "../assets/logo_vertical_parana.png"
+
 const HomepageContainer = () => {
     return ( 
         <>
+            <div className="logo-container">
+                <img src={logo} alt="logo" id="home-logo"></img>
+            </div>
             <p>Hello from Homepage Container</p>
         </>
      );
