c0c7eaf729d611f0f843f57ef6d62fe879b901b7
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-04-11 11:25:46 +0100

    "added patch request for Truck availability"

--- server/src/main/java/com/example/demo/controllers/TruckController.java
+++ server/src/main/java/com/example/demo/controllers/TruckController.java
@@ -1,15 +1,13 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.AvailabilityEnum;
 import com.example.demo.models.Truck;
 import com.example.demo.services.TruckService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,5 +34,15 @@ public class TruckController {
         } else {
             return new ResponseEntity<>(truck, HttpStatus.OK);
         }
+    }
+
+    @PatchMapping(value = "/{id}/status")
+    public ResponseEntity<Optional<Truck>> changeTruckAvailability(@RequestBody AvailabilityEnum truckStatus, @PathVariable long id){
+        Optional<Truck> truckToUpdate = truckService.getTruckById(id);
+    if(truckToUpdate.isPresent()) {
+        Optional<Truck> newTruck = truckService.changeTruckAvailability(truckStatus,id);
+        return new ResponseEntity<>(newTruck, HttpStatus.OK);
+    }
+    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
