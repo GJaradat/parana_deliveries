599442c2d835e17c12efad7cd61d3e63ad7ee86f
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-04-11 09:48:37 +0100

    changes request mapping value

--- server/src/main/java/com/example/demo/controllers/TruckController.java
+++ server/src/main/java/com/example/demo/controllers/TruckController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/chocolates")
+@RequestMapping(value = "/trucks")
 public class TruckController {
 
     @Autowired
