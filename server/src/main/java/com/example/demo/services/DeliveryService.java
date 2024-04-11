00090c3db2abc8b59e5b61d51e0e1ad5218850a3
Kajanan <Kajanan15i@gmail.com>
2024-04-11 10:25:17 +0100

    created fnuctions for getting all deliveries and a specific delivery by id

---
+++ server/src/main/java/com/example/demo/services/DeliveryService.java
@@ -0,0 +1,26 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Delivery;
+import com.example.demo.repositories.DeliveryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DeliveryService {
+
+    @Autowired
+    DeliveryRepository deliveryRepository;
+
+
+    public List<Delivery> getAllDeliveries(){
+        return deliveryRepository.findAll();
+    }
+
+    public Optional<Delivery> getDeliveryById(long id){
+        return deliveryRepository.findById(id);
+    }
+    
+}
