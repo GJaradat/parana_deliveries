e9bdabe13fdc11ead98f75aa0b0f99eebdbbcb80
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-04-11 10:08:38 +0100

    creates get truck by id method in truck service and GET Mapping in controller

--- server/src/main/java/com/example/demo/controllers/TruckController.java
+++ server/src/main/java/com/example/demo/controllers/TruckController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/trucks")
@@ -24,5 +26,15 @@ public class TruckController {
     public ResponseEntity<List<Truck>> getAllTrucks(){
         List<Truck> truck = truckService.getAllTrucks();
         return new ResponseEntity<>(truck, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Truck>> getTruckById(@PathVariable long id){
+        Optional<Truck> truck = truckService.getTruckById(id);
+        if(truck.isEmpty()){
+            return new ResponseEntity<>(truck, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(truck, HttpStatus.OK);
+        }
     }
 }
--- server/src/main/java/com/example/demo/services/TruckService.java
+++ server/src/main/java/com/example/demo/services/TruckService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TruckService {
@@ -17,4 +18,7 @@ public class TruckService {
         return truckRepository.findAll();
     }
 
+    public Optional<Truck> getTruckById(Long id){
+        return truckRepository.findById(id);
+    }
 }
