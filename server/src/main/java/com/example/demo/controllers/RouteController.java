2feae9bb65ac5e51fee6c71dc4168d0327976ebd
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-04-11 10:28:19 +0100

    added index endpoint for routes

--- server/src/main/java/com/example/demo/controllers/RouteController.java
+++ server/src/main/java/com/example/demo/controllers/RouteController.java
@@ -1,4 +1,27 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Route;
+import com.example.demo.services.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/routes")
 public class RouteController {
+
+    @Autowired
+    RouteService routeService;
+
+    @GetMapping
+    public ResponseEntity<List<Route>> getRoutes(){
+        List<Route> routeList = routeService.getAllRoutes();
+        return new ResponseEntity<>(routeList, HttpStatus.OK);
+    }
+
 }
--- server/src/main/java/com/example/demo/services/RouteService.java
+++ server/src/main/java/com/example/demo/services/RouteService.java
@@ -1,13 +1,21 @@
 package com.example.demo.services;
 
+import com.example.demo.models.Route;
 import com.example.demo.repositories.RouteRepository;
 import com.example.demo.repositories.TruckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RouteService {
+
     @Autowired
     RouteRepository routeRepository;
+
+    public List<Route> getAllRoutes() {
+        return routeRepository.findAll();
+    }
 
 }
