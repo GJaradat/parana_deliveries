9a986940b2cf7c7bcfd38f3cf25e7e40dec4a209
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-04-12 10:59:17 +0100

    Displays routes and route details on delivery routes page

--- client/src/containers/RoutesContainer.jsx
+++ client/src/containers/RoutesContainer.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import RouteList from '../components/RouteList'
 import RouteMap from '../components/RouteMap';
 
 const RoutesContainer = () => {
+    
+    const [routes, setRoutes] = useState([]);
+
+    const loadRoutes = async () => {
+        const response = await fetch("http://localhost:8080/routes");
+        const jsonData = await response.json();
+        setRoutes(jsonData);
+    }
+
+    useEffect(()=> {
+        loadRoutes();
+    }, []);
+    
     return ( 
         <>
-            <p>Hello from Routes Container</p>
-            <RouteList />
+            <h2>Delivery Routes</h2>
+            <RouteList routes={routes} />
             <RouteMap />
         </>
      );
