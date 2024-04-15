00c3292c33614c7232a4e32be735b23023107991
Leila Peltier <l_peltier@outlook.com>
2024-04-15 10:06:40 +0100

    done skeleton for updating truck availability status

--- client/src/components/Truck.jsx
+++ client/src/components/Truck.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import '../styles/TruckStyle.css';
 const Truck = ({ truck }) => {
-    
+
+    const[availability, setAvailability] = useState(truck.availability);
+
     
     const availabilityStatus = () => {
         if(truck.availability === "IN_DEPOT"){
@@ -22,7 +25,18 @@ const Truck = ({ truck }) => {
                 <h3>Truck: {truck.name}</h3>
                 <p>Capacity: {truck.capacity}</p>
                 <p>Availability: {availabilityStatus()}</p>
+
+                <select name="editAvailabilityDropdown" 
+                id="editAvailabilityDropdown" 
+                // defaultValue={truck.availability} 
+                onChange={((e)=>setAvailability(e.target.value))}>
+                    <option value="IN_DEPOT"> In Depot</option>
+                    <option value="OUT_FOR_DELIVERY">Out For Delivery</option>
+                    <option value="UNDER_MAINTENANCE"> Under Maintenance</option>
+                </select>
+              
                 </article>
+
         </>
      );
 }
