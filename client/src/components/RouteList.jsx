155e39d1a911827e71360cd7617a937abec0f879
Leila Peltier <l_peltier@outlook.com>
2024-04-15 14:46:21 +0100

    Truck and Route components no longer change order when updating their statuses!

--- client/src/components/RouteList.jsx
+++ client/src/components/RouteList.jsx
@@ -2,7 +2,7 @@ import Route from './Route'
 
 const RouteList = ({routes, patchRoutes}) => {
 
-    const mappedRoutes = routes.map((route)=>{
+    const mappedRoutes = routes.sort((a, b) => a.id - b.id).map((route)=>{
         return <Route key={route.id} route={route} patchRoutes={patchRoutes} />
     });
 
