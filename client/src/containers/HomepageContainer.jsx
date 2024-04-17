02508fe40b862846321846ad16dc38408eb7fc79
Leila Peltier <l_peltier@outlook.com>
2024-04-17 09:37:10 +0100

    Adds styling + useEffect logic to remove the header at the top when on the homepage

--- client/src/containers/HomepageContainer.jsx
+++ client/src/containers/HomepageContainer.jsx
@@ -1,11 +1,22 @@
 import Navigation from "../components/Navigation";
-import logo from "../assets/logo_vertical_parana.png"
+import logo from "../assets/logo_vertical_parana.png";
+import "../styles/HomePageStyle.css";
+import { useEffect } from "react";
 
 
 const HomepageContainer = () => {
+    useEffect(() => {
+        document.body.classList.add('hide-header');
+
+        // Cleanup function to remove the class when the component unmounts
+        return () => {
+          document.body.classList.remove('hide-header');
+        };
+     }, []);
+
     return ( 
         <>
-            <div className="logo-container">
+            <div id="logo-container">
                 <img src={logo} alt="logo" id="home-logo"></img>
             </div>
             <p>Hello from Homepage Container</p>
