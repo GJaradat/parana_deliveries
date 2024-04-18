a642a32775b9a32d0ba5178774c4e343eebf727e
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-04-18 15:49:42 +0100

    removed dependencies

--- client/src/containers/DeliveriesContainer.jsx
+++ client/src/containers/DeliveriesContainer.jsx
@@ -25,6 +25,8 @@ const DeliveriesContainer = () => {
             <section id="deliveries-map">
                 <RouteMap routes={null} deliveries={deliveries}/>
             </section>
+        </section>
+
     );
 }
  
